839e1e5e190b88224d3d14800738ffe86087b2b8
Tanmay Gulati <tanmaygulati@Tanmays-MacBook-Air.local>
2017-08-22 11:47:57 +0530

    Begining of Website Blocker

---
+++ Website Blocker/WebsiteBlocker.py
@@ -0,0 +1,9 @@
+import time
+from datetime import datetime as dt
+hosts_path = r"/etc/hosts"
+redirect = "127.0.0.1"
+website_lists = ["www.facebook.com", "facebook.com"]
+
+while True:
+	print(1)
+	time.sleep(5)
