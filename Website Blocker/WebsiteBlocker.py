82acf047b4b600d90db1c872d3ee18e8cb20bbe0
Tanmay Gulati <tanmaygulati@Tanmays-MacBook-Air.local>
2017-08-26 14:55:43 +0530

    Midway through website blocker

--- Website Blocker/WebsiteBlocker.py
+++ Website Blocker/WebsiteBlocker.py
@@ -5,5 +5,8 @@ redirect = "127.0.0.1"
 website_lists = ["www.facebook.com", "facebook.com"]
 
 while True:
-	print(1)
-	time.sleep(5)
+	if dt(dt.now().year, dt.now().month, dt.now().day, 10) < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+		print(1)
+	else:
+		print("Fun hours")
+	time.sleep(1)
